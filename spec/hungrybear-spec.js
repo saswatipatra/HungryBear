2b68e398ba551c66643c1aec47efb4263ea0da88
saswati patra and Uriel Gonzalez <saswati@>
2019-07-01 14:51:17 -0700

    Closures in JavaScript

--- spec/hungrybear-spec.js
+++ spec/hungrybear-spec.js
@@ -1,49 +1,73 @@
-import { HungryBear } from './../src/hungrybear.js';
+// import { HungryBear } from './../src/hungrybear.js';
+//
+// describe('HungryBear', function() {
+//   let fuzzy = new HungryBear("Fuzzy");
+//
+//   beforeEach(function() {
+//     jasmine.clock().install();
+//     fuzzy.setHunger();
+//   });
+//
+//   afterEach(function() {
+//     jasmine.clock().uninstall();
+//   });
+//
+//   it('should have a name and a food level of 10 when it is created', function() {
+//     expect(fuzzy.name).toEqual("Fuzzy");
+//     expect(fuzzy.foodLevel).toEqual(10);
+//   });
+//
+//
+//
+//   it('should have a food level of 7 after 3001 milliseconds', function() {
+//     jasmine.clock().tick(3001);
+//     expect(fuzzy.foodLevel).toEqual(7);
+//   });
+//
+//   it('should get very hungry if the food level drops below zero and you are dead', function() {
+//     fuzzy.foodLevel = 0;
+//     expect(fuzzy.dead()).toEqual(true);
+//   });
+//
+//   it('should get very hungry if the food level drops below zero', function() {
+//     fuzzy.foodLevel = 0;
+//     expect(fuzzy.didYouGetEaten()).toEqual(true);
+//   });
+//
+//   it('should get very hungry if 10 seconds pass without feeding', function() {
+//     jasmine.clock().tick(10001);
+//     expect(fuzzy.didYouGetEaten()).toEqual(true);
+//   });
+//
+//
+//
+//   it('should have a food level of ten if it is fed', function() {
+//     jasmine.clock().tick(9001);
+//     fuzzy.feed();
+//     expect(fuzzy.foodLevel).toEqual(10);
+//   });
+// });
+
+
+import { bear } from './../src/hungrybear.js';
 
 describe('HungryBear', function() {
-  let fuzzy = new HungryBear("Fuzzy");
+  let fuzzy = bear;
 
   beforeEach(function() {
     jasmine.clock().install();
+    fuzzy.foodLevel = 10;
+    fuzzy.name = "Fuzzy";
     fuzzy.setHunger();
   });
 
-  afterEach(function() {
-    jasmine.clock().uninstall();
+  it('should return that the bear ate blueberries and the food level should go up 5', function() {
+      expect(fuzzy.eatSmall("blueberries")).toEqual("The bear ate the blueberries! Food level goes up 5!");
+      expect(fuzzy.foodLevel).toEqual(15);
+    });
+
+  it('should return that the bear ate blueberries and the food level should go up 10'), function() {
+      expect(fuzzy.eatMedium("blueberries")).toEqual("The bear ate the blueberries! Food level goes up 10!");
+      expect(fuzzy.foodLevel).toEqual(25);
+    });
   });
-
-  it('should have a name and a food level of 10 when it is created', function() {
-    expect(fuzzy.name).toEqual("Fuzzy");
-    expect(fuzzy.foodLevel).toEqual(10);
-  });
-
-
-
-  it('should have a food level of 7 after 3001 milliseconds', function() {
-    jasmine.clock().tick(3001);
-    expect(fuzzy.foodLevel).toEqual(7);
-  });
-
-  it('should get very hungry if the food level drops below zero and you are dead', function() {
-    fuzzy.foodLevel = 0;
-    expect(fuzzy.dead()).toEqual(true);
-  });
-  
-  it('should get very hungry if the food level drops below zero', function() {
-    fuzzy.foodLevel = 0;
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
-
-  it('should get very hungry if 10 seconds pass without feeding', function() {
-    jasmine.clock().tick(10001);
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
-
-
-
-  it('should have a food level of ten if it is fed', function() {
-    jasmine.clock().tick(9001);
-    fuzzy.feed();
-    expect(fuzzy.foodLevel).toEqual(10);
-  });
-});
--- src/hungrybear.js
+++ src/hungrybear.js
@@ -1,39 +1,66 @@
-export class HungryBear {
+// export class HungryBear {
+//
+//   constructor(name) {
+//     this.name = name;
+//     this.foodLevel = 10;
+//   }
+//
+//   setHunger() {
+//     setInterval(() => {
+//       this.foodLevel--;
+//     }, 1000);
+//   }
+//
+//   didYouGetEaten() {
+//     if (this.foodLevel > 0) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+//
+//   dead() {
+//    if (this.foodLevel > 0) {
+//     return false ;
+//   } else {
+//     return true;
+//   }
+// }
+//
+// feed() {
+//     this.foodLevel = 10;
+//
+// }
+//
+// }
 
-  // setInterval(function(){
-  //   //check and display time every second!
-  // }, 1000);
 
-  constructor(name) {
-    this.name = name;
-    this.foodLevel = 10;
-  }
-
-  setHunger() {
-    setInterval(() => {
+export let bear = {
+  foodLevel: 10,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => {
       this.foodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(hungerInterval);
+        return "You got eaten!";
+      }
     }, 1000);
-  }
-
-  didYouGetEaten() {
+  },
+  didYouGetEaten: function() {
     if (this.foodLevel > 0) {
       return false;
     } else {
       return true;
     }
+  },
+  feed: function(amount) {
+    let that = this;
+    return function(food) {
+      that.foodLevel += amount
+      return `The bear ate the ${food}! Food level goes up ${amount}!`
+    }
   }
+};
 
-  dead() {
-   if (this.foodLevel > 0) {
-    return false ;
-  } else {
-    return true;
-  }
-}
-
-feed() {
-    this.foodLevel = 10;
-
-}
-
-}
+bear.eatSmall = bear.feed(5);
+bear.eatMedium = bear.feed(10):
